c5d441344bac7b89862704492bf05d2801c9814f
SuperSection <soumosarkar297@gmail.com>
2024-12-05 22:14:09 +0530

    chore: setup vite project with tailwindcss and shadcn-ui

---
+++ eslint.config.js
@@ -0,0 +1,82 @@
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+// eslint-disable-next-line import/no-unresolved
+import tseslint from 'typescript-eslint';
+import _import from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  ...fixupConfigRules(
+    compat.extends(
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:import/recommended',
+      'plugin:prettier/recommended',
+    ),
+  ),
+  {
+    // extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      import: fixupPluginRules(_import),
+      prettier: fixupPluginRules(prettier),
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    settings: {
+      'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+        alias: {
+          map: [['@', './src']],
+          extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        },
+      },
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['off', { allowConstantExport: true }],
+
+      'import/order': [
+        'error',
+        {
+          groups: [
+            ['builtin', 'external'],
+            ['internal', 'parent', 'sibling', 'index'],
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'always', // Ensure newlines between groups
+        },
+      ],
+
+      'prettier/prettier': [
+        'off',
+        {
+          endOfLine: 'auto',
+          parser: 'flow',
+        },
+      ],
+    },
+  },
+);
